5a9f67fa607731e3c7db2a76ce0424ed0708eec0
bamb1sh0 <pro100ladikalexey@gmail.com>
2021-11-03 22:21:39 +0300

    Update Work1.java

--- src/com/company/Work1.java
+++ src/com/company/Work1.java
@@ -5,23 +5,32 @@ import java.util.Random;
 //Дан массив n целых чисел, содержащий не менее двух нулей.
 //Найти сумму чисел, расположенных между последними двумя нулями.
 public class Work1 {
-    static int size = 5;
+    static int size = 10;
     static int[] arr = new int[size];
 
     public static void main(String[] args) {
-        arr = new int[]{0, 1, 2, 3, 0};
+        arr = new int[]{1, 1, 2, 3, 0,1,1,1,3,0};
         int sum = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-                if (arr[i] != 0){
-                sum += arr[i];}
+        int min = 0;
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i]==0) {
+                min=i; //b[i];
+                break;
+            }
+        }
+        int max = arr.length;
+        for (i = min+1; i < args.length; i++) {
+            if (arr[i]==0) {
+                max=i;//b[i];
+                break;
+            }
+        }
+        for (i = min+1; i < max; i++){
+            sum += arr[i];
 
         }
         System.out.println(sum);
     }
-
-
-
-
 }
 
